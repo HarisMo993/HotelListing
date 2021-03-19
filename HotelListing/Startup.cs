ac9f5f72e450741949f9b945a3574ebb358da5ff
haris1993 <harisagic@hotmail.com>
2021-03-19 01:21:13 +0100

    Added Database, AutoMapper, Unit of Work and DTOs

--- HotelListing/Startup.cs
+++ HotelListing/Startup.cs
@@ -1,7 +1,10 @@
+using HotelListing.Configurations;
+using HotelListing.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -27,6 +30,9 @@ namespace HotelListing
         public void ConfigureServices(IServiceCollection services)
         {
 
+            services.AddDbContext<DatabaseContext>(options => 
+                options.UseSqlServer(Configuration.GetConnectionString("SqlConnection")));
+
             services.AddControllers();
 
             services.AddCors(o => {
@@ -35,6 +41,8 @@ namespace HotelListing
                     .AllowAnyMethod()
                     .AllowAnyHeader());
             });
+
+            services.AddAutoMapper(typeof(MapperInitilizer));
 
             services.AddSwaggerGen(c =>
             {
